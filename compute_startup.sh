d1715fcca729c85dbcb9129c3f6d7b5e1bf0cd1b
PyxieLouStar <pyxiestar@gmail.com>
2024-01-25 12:45:27 -0800

    add files
    add cluster template, cluster json, and cluster-init files

---
+++ compute_startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get -y install build-essential
+sudo apt-get -y install ghostscript
+# sudo apt-get -y install unzip
+
+sudo apt-get -y install libxinerama1
+sudo apt-get -y  install libxt6
+sudo apt-get -y  install libxmu6
